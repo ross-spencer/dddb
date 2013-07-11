9f1008378abd3620748cce64bc88d24d1f0cc129
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2013-07-11 19:46:40 +1200

    [bugfix] Problem accessing mysql data on init()?

--- public/sparql/endpoint.php
+++ public/sparql/endpoint.php
@@ -1,40 +1,38 @@
 <?php
 
-/* ARC2 static class inclusion */ 
-include_once("arc/ARC2.php");
+	/* ARC2 static class inclusion */ 
+	include_once("arc/ARC2.php");
 
 
-/* MySQL and endpoint configuration */ 
-$config = array(
-  /* db */
-  'db_host' => 'localhost', /* optional, default is localhost */
-  'db_name' => 'arc_db',
-  'db_user' => 'root',
-  'db_pwd' => 'root',
+	/* MySQL and endpoint configuration */ 
+	$config = array(
+	  /* db */
+	  'db_host' => 'localhost', /* optional, default is localhost */
+	  'db_name' => 'arc_db',
+	  'db_user' => 'root',
+	  'db_pwd' => 'root',
 
-  /* store name */
-  'store_name' => 'arc_tests',
+	  /* store name */
+	  'store_name' => 'dddb_db',
 
-  /* endpoint */
-  'endpoint_features' => array(
-    'select', 'construct', 'ask', 'describe',  
-    'dump' /* dump is a special command for streaming SPOG export */
-  ),
-  'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
-  'endpoint_read_key' => '', /* optional */
-  'endpoint_max_limit' => 250, /* optional */
-);
+	  /* endpoint */
+	  'endpoint_features' => array(
+		 'select', 'construct', 'ask', 'describe',  
+		 'dump' /* dump is a special command for streaming SPOG export */
+	  ),
+	  'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
+	  'endpoint_read_key' => '', /* optional */
+	  'endpoint_max_limit' => 250, /* optional */
+	);
 
-/* instantiation */
-$ep = ARC2::getStoreEndpoint($config);
+	/* instantiation */
+	$ep = ARC2::getStoreEndpoint($config);
 
-if (!$ep->isSetUp()) {
-  $ep->setUp(); /* create MySQL tables */
-}
+	#if (!$ep->isSetUp()) {								# query use of this
+	#  $ep->setUp(); /* create MySQL tables */
+	#}
 
-/* request handling */
-$ep->go();
-
-
+	/* request handling */
+	$ep->go();
 
 ?>
